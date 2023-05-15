05c1ecb2ffee8644b748066ed6415c8b55af5e99
Bruno Ferreira <contatobrunokye@gmail.com>
2023-05-15 20:18:17 -0300

    feat: requisito 16

--- app/backend/src/controllers/Match.controller.ts
+++ app/backend/src/controllers/Match.controller.ts
@@ -2,7 +2,16 @@ import { Request, Response } from 'express';
 import MatchService from '../services/Match.service';
 
 export default class MatchController {
-  public static async getMatches(_req: Request, res: Response): Promise<Response | void> {
+  public static async getMatches(req: Request, res: Response): Promise<Response | void> {
+    const { inProgress } = req.query;
+
+    if (inProgress) {
+      const status = inProgress as string;
+      const matches = await MatchService.getInProgress(status);
+
+      return res.status(200).json(matches);
+    }
+
     const matches = await MatchService.getMatches();
     res.status(200).json(matches);
   }
--- app/backend/src/services/Match.service.ts
+++ app/backend/src/services/Match.service.ts
@@ -11,4 +11,14 @@ export default class MatchService {
       ],
     });
   }
+
+  public static async getInProgress(status: string): Promise<IMatch[]> {
+    return MatchModel.findAll({
+      include: [
+        { model: TeamModel, as: 'homeTeam', attributes: ['teamName'] },
+        { model: TeamModel, as: 'awayTeam', attributes: ['teamName'] },
+      ],
+      where: { inProgress: status === 'true' },
+    });
+  }
 }
